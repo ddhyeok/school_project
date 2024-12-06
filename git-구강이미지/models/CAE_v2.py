697c995af6ae4c47000ac0632a12aa4ab6fbec17
DongHyeok Kim <115151226+ddhyeok@users.noreply.github.com>
2024-12-06 20:24:23 +0900

    Add files via upload

---
+++ git-구강이미지/models/CAE_v2.py
@@ -0,0 +1,127 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# resnet 18,34  
+class ResNetBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1, dropout=0.3):
+        super(ResNetBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.dropout = nn.Dropout(dropout)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_channels != out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride),
+                nn.BatchNorm2d(out_channels)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        
+        out = self.dropout(out)
+        out = self.bn2(self.conv2(out))
+        
+        out =out+ self.shortcut(x)
+        
+        out = F.relu(out)
+        return out
+    
+
+class Encoder(nn.Module):
+    def __init__(self):
+        super(Encoder, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.pool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        # ResNet
+        self.layer1 = ResNetBlock(64, 64)
+        self.layer2 = ResNetBlock(64, 64)
+        
+        self.layer3 = ResNetBlock(64, 128, stride=2)#sc
+        
+        self.layer4 = ResNetBlock(128, 128)
+        
+        self.layer5 = ResNetBlock(128, 256, stride=2)#sc
+        
+        self.layer6 = ResNetBlock(256, 256)
+        
+        self.layer7 = ResNetBlock(256, 512, stride=2)#sc
+        
+        self.layer8 = ResNetBlock(512, 512)
+
+        # fc
+        self.fc1 = nn.Linear(512 * 6 * 12, 1024)
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        #96 192 
+        x = self.pool(x)
+        #48 96
+
+        # 
+        x = self.layer1(x)
+        x = self.layer2(x)
+        
+        x = self.layer3(x)#
+        #24 48
+        x = self.layer4(x)
+        x = self.layer5(x)#
+        #12 24
+        x = self.layer6(x)
+        x = self.layer7(x)#
+        #6 12
+        x = self.layer8(x)
+
+        # fc
+        x = x.view(x.size(0), -1)  
+        x = F.relu(self.fc1(x))
+        return x
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super(Decoder, self).__init__()
+        # 복원
+        self.fc2 = nn.Linear(1024, 512 * 6 * 12)
+
+        self.deconv1 = nn.ConvTranspose2d(512, 256, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.bn1 = nn.BatchNorm2d(256)
+        
+        self.deconv2 = nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.bn2 = nn.BatchNorm2d(128)
+        
+        self.deconv3 = nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.bn3 = nn.BatchNorm2d(64)
+        
+        self.deconv4 = nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.bn4 = nn.BatchNorm2d(32)
+        
+        self.deconv5 = nn.ConvTranspose2d(32, 3, kernel_size=7, stride=2, padding=3, output_padding=1)
+    
+    def forward(self, x):
+        x = F.relu(self.fc2(x))
+        x = x.view(x.size(0), 512, 6, 12)  
+        # 6,12,512
+        
+        x = F.relu(self.bn1(self.deconv1(x)))#12 24
+        x = F.relu(self.bn2(self.deconv2(x)))#24 48
+        x = F.relu(self.bn3(self.deconv3(x)))#48 96
+        x = F.relu(self.bn4(self.deconv4(x)))#96 192
+        
+        x = F.sigmoid(self.deconv5(x))  #192 384 0~1
+        return x
+
+class CAE(nn.Module):
+    def __init__(self):
+        super(CAE, self).__init__()
+        self.encoder = Encoder()
+        self.decoder = Decoder()
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
